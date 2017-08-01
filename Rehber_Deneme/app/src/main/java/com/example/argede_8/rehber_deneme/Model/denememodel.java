7c05cb587451f735be6f314415042c4f4b531a25
yenenahmet <yenenahmet@hotmail.com>
2017-08-01 23:49:55 +0300

    Update denememodel.java

--- Rehber_Deneme/app/src/main/java/com/example/argede_8/rehber_deneme/Model/denememodel.java
+++ Rehber_Deneme/app/src/main/java/com/example/argede_8/rehber_deneme/Model/denememodel.java
@@ -1,11 +1,8 @@
-package com.example.argede_8.rehber_deneme.Model;
+
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by ARGEDE_8 on 5/6/2017.
- */
 
 public class denememodel {
     @SerializedName("Elma")
