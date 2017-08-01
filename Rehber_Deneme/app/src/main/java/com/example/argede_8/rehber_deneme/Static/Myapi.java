b98aadf663656bd68c2e9b82ca008552e3b2b23a
yenenahmet <yenenahmet@hotmail.com>
2017-08-01 23:45:08 +0300

    Update Myapi.java

--- Rehber_Deneme/app/src/main/java/com/example/argede_8/rehber_deneme/Static/Myapi.java
+++ Rehber_Deneme/app/src/main/java/com/example/argede_8/rehber_deneme/Static/Myapi.java
@@ -1,8 +1,5 @@
-package com.example.argede_8.rehber_deneme.Static;
 
-/**
- * Created by ARGEDE_8 on 5/6/2017.
- */
+
 
 public class Myapi {
     public static final String URL ="http://192.168.43.201:90";
